6b8ee360091b243f5738d1eb9418994cfd269c7c
darik88 <mr.darik88@gmail.com>
2020-11-04 21:36:15 +0300

    Initial Commit

---
+++ HW2.7-PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.7-PersonsList
+//
+//  Created by Айдар Рахматуллин on 04.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
